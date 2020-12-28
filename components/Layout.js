8af720e78c261cf5cdd45f301899b555bddf4b9b
ashhar001 <ashar.ansari1020@gmail.com>
2020-12-28 20:04:54 +0530

    Upto Form in /campaigns/new lec25

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import  React from 'react';
+import { Container } from 'semantic-ui-react';
+// import 'semantic-ui-css/semantic.min.css';
+import Head from 'next/head';
+import Header from './Header';
+
+
+export default props =>{
+
+    return(
+        <Container>
+            <Head>
+            <link
+                rel="stylesheet"
+                href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+            />
+            </Head>
+            <Header/>
+            
+            {props.children}
+            
+        </Container>
+    );
+}
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -2,7 +2,9 @@ import Web3 from 'web3';
 
 let web3;
 
-if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
+//if will handle the code being executed inside the browser and metamask is availabe
+//type of is used to see if a variabe is defined
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {  
   // We are in the browser and metamask is running.
   web3 = new Web3(window.web3.currentProvider);
 } else {
